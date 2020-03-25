6e8f75d27b966cc6b75cabc9750148dadf739bbf
Tomás Augusto Ralph <41462117+tralph3@users.noreply.github.com>
2020-03-25 15:02:15 -0300

    Add files via upload

--- CueMaker.py
+++ CueMaker.py
@@ -4,12 +4,13 @@ import glob
 directory = input("Input the absolute path to the folder where the bin files are located: ")
 os.chdir(directory)
 
-binFiles = glob.glob("*.bin")
+binFiles = glob.glob("**/*.bin", recursive=True)
 
 def makeCue():
     for file in binFiles:
-        cue = open(file[0:len(file) - 4] + ".cue", "w+")
-        cueText = "FILE \"" + file + "\" BINARY\n  TRACK 01 MODE2/2352\n    INDEX 01 00:00:00\n"
+        fileDirectory = file[0:file.rfind("/") + 1]
+        cue = open(fileDirectory + file[file.rfind("/"):len(file) - 4] + ".cue", "w+")
+        cueText = "FILE \"" + file[file.rfind("/") + 1:len(file)] + "\" BINARY\n  TRACK 01 MODE2/2352\n    INDEX 01 00:00:00\n"
         cue.write(cueText)
 
 makeCue()
