0ea2945078b3c95af36746a72a49e8c6668d6899
tralph3 <tomasralph2000@gmail.com>
2020-06-23 06:27:12 -0300

    Added Sega Saturn support + general improvements

--- CueMaker.py
+++ CueMaker.py
@@ -20,11 +20,18 @@ def getSha1(file):
     return hashSha1.hexdigest()
 
 try:
-    hashFile = open("psx.hash", "r").read()
-except FileNotFoundError:
-    print("\u001b[0;31mError: Can't find \"psx.hash\", make sure it's on the same folder as the script\u001b[0m")
-    exit()
-
+    link = 'https://raw.githubusercontent.com/tralph3/Cue-Maker/master/psx.hash'
+    psxHashFile = urlopen(link).read().decode("UTF-8")
+    link = 'https://raw.githubusercontent.com/tralph3/Cue-Maker/master/saturn.hash'
+    saturnHashFile = urlopen(link).read().decode("UTF-8")
+except Exception:
+    try:
+        print("\u001b[0;31mError: Can't connect to GitHub, defaulting to local hash file.\u001b[0m")
+        psxHashFile = open("psx.hash", "r").read()
+        saturnHashFile = open("saturn.hash", "r").read()
+    except FileNotFoundError:
+        print("\u001b[0;31mError: Can't find hash file, make sure they are on the same folder as the script.\u001b[0m")
+        exit()
 if len(sys.argv) < 2 or len(sys.argv) > 4:
     print("\u001b[1;31mUsage:\n----------\u001b[0m\n\n")
     print("\u001b[0;32;40m$ python3 CueMaker.py <directory> <recursive> <generic-cues>\u001b[0m\n")
@@ -61,13 +68,17 @@ matchingFiles = []
 cueFiles = []
 m3uWriteCounter = 0
 cueCreatedCounter = 0
+currentGameCue = ""
+currentGameCuePath = ""
 
 if recursive.lower() == "y":
     for files in types:
         matchingFiles.extend(glob.glob("**/" + files, recursive=True))
+        matchingFiles = sorted(matchingFiles)
 elif recursive.lower() == "n":
     for files in types:
         matchingFiles.extend(glob.glob(files))
+        matchingFiles = sorted(matchingFiles)
 else:
     print("\u001b[0;31mError: Bad argument, only \"y\" or \"n\" accepted\u001b[0m")
     exit()
@@ -75,24 +86,35 @@ else:
 print("\u001b[1;32mCreating .cue...\u001b[0m\n")
 
 def createGenericCue(cuePath, fileName):
+    global cueCreatedCounter
     cue = open(cuePath, "w+")
     cueText = "FILE \"" + fileName + "\" BINARY\n  TRACK 01 MODE2/2352\n    INDEX 01 00:00:00\n"
     cue.write(cueText)
     cue.close()
     print("\u001b[36mCreated: \"\u001b[1;33m" + fileName[0:len(fileName) - 4] + ".cue\u001b[36m\"\u001b[0m\n--------")
-    global cueCreatedCounter
     cueCreatedCounter += 1
 
-def fetchCue(entryName):
+def fetchCue(entryName, system):
     #Creates links given an entry name
-    try:
-        link = 'https://raw.githubusercontent.com/tralph3/Cue-Maker/master/Sony PlayStation Cue Sheets (redump.org)/' + entryName[:len(entryName)-4] + ".cue"
-        link = link.replace(" ", "%20")
-        cueText = urlopen(link).read().decode("UTF-8")
-    except Exception:
-        link = 'https://raw.githubusercontent.com/tralph3/Cue-Maker/master/Sony PlayStation Cue Sheets (emuparadise.me)/' + entryName[:len(entryName)-4] + ".cue"
-        link = link.replace(" ", "%20")
-        cueText = urlopen(link).read().decode("UTF-8")
+    if system == "PlayStation":
+        try:
+            link = 'https://raw.githubusercontent.com/tralph3/Cue-Maker/master/Sony PlayStation Cue Sheets (redump.org)/' + entryName[:len(entryName)-4] + ".cue"
+            link = link.replace(" ", "%20")
+            cueText = urlopen(link).read().decode("UTF-8")
+        except Exception:
+            try:
+                link = 'https://raw.githubusercontent.com/tralph3/Cue-Maker/master/Sony PlayStation Cue Sheets (emuparadise.me)/' + entryName[:len(entryName)-4] + ".cue"
+                link = link.replace(" ", "%20")
+                cueText = urlopen(link).read().decode("UTF-8")
+            except Exception:
+                return Exception
+    elif system == "Saturn":
+        try:
+            link = 'https://raw.githubusercontent.com/tralph3/Cue-Maker/master/Sega Saturn Cue Sheets (redump.org)/' + entryName[:len(entryName)-4] + ".cue"
+            link = link.replace(" ", "%20")
+            cueText = urlopen(link).read().decode("UTF-8")
+        except Exception:
+            return Exception
     return cueText
 
 def getTrackNumber(entryName):
@@ -100,7 +122,7 @@ def getTrackNumber(entryName):
     spaceIndex = entryName.rfind(" ", trackIndex, len(entryName))
     parenthesisIndex = entryName.rfind(")", trackIndex, len(entryName))
     trackNumber = entryName[spaceIndex+1:parenthesisIndex]
-    return str(trackNumber.zfill(1))
+    return str(trackNumber).zfill(2)
 
 def replaceCueFileName(cueText, fileName):
     nameIndex = cueText.find("\"")
@@ -109,6 +131,9 @@ def replaceCueFileName(cueText, fileName):
     return cueText
 
 def generateCue(file):
+    global currentGameCue
+    global currentGameCuePath
+    global cueCreatedCounter
     #Ignore .chd files
     if file.rfind(".chd") != -1:
         cueFiles.append(file)
@@ -127,75 +152,96 @@ def generateCue(file):
     if not os.path.isfile(cuePath):
         print("\u001b[1;32mGenerating SHA-1 hash...\u001b[0m\n")
         fileHash = getSha1(file)
-        foundEntry = hashFile.find(fileHash)
+        foundEntry = psxHashFile.find(fileHash)
+
         if foundEntry == -1:
-            if fileName.rfind("Track") != -1:
-                print("\u001b[1;31mCouldn't find database entry, track file detected, not generating .cue...\u001b[0m")
-                return None
+            foundEntry = saturnHashFile.find(fileHash)
+            system = saturnHashFile
+            systemName = "Saturn"
+
+            if foundEntry == -1:
+
+                if fileName.lower().rfind("track") != -1:
+                    print("\u001b[1;31mCouldn't find database entry, track file detected, not generating .cue...\u001b[0m")
+                    return None
+                else:
+
+                    if genericCues:
+                        print("\u001b[1;31mCouldn't find database entry, creating generic .cue...\u001b[0m")
+                        createGenericCue(cuePath, fileName)
+                        cueFiles.append(cuePath)
+                    else:
+                        print("\u001b[1;31mCouldn't find database entry.\u001b[0m\n--------")
+        else:
+            system = psxHashFile
+            systemName = "PlayStation"
+
+        #Create an entry name based by a fixed offset from the hash
+        print("\u001b[1;33mHash \u001b[1;32m" + fileHash + "\u001b[1;33m matches database!")
+        lineEnd = system.find("\n", foundEntry)
+        entryName = system[int(foundEntry + len(fileHash) + 2):int(lineEnd)]
+        #If the file has multiple tracks, use special conditions
+        if entryName.rfind("Track ") != -1:
+            #Modify entry name to get the appriate link
+            trackNumber = getTrackNumber(entryName)
+            entryName = entryName.replace(" (Track " + str(int(trackNumber)) + ").bin", ".cue")
+            if entryName[-4:] == ".bin":
+                entryName = entryName.replace(" (Track " + trackNumber + ").bin", ".cue")
+            if int(trackNumber) == 1:
+                #Fetch the cue and save its entirety into the global variable "currentGameCue"
+                cueText = fetchCue(entryName, systemName)
+                if cueText == Exception:
+                    print("\u001b[1;31mCouldn't find original cue on GitHub.\u001b[0m\n--------")
+                    return None
+                currentGameCue = cueText
+                trackIndex = cueText.find("TRACK " + trackNumber)
+                fileIndex = cueText.rfind("FILE \"", 0, trackIndex)
+                nextFileIndex = cueText.find("FILE \"", trackIndex)
+                trackCueText = cueText[fileIndex:nextFileIndex]
+                trackCueText = replaceCueFileName(trackCueText, fileName)
+                cuePathTrackIndex = cuePath.lower().rfind(" (track")
+                cuePathParenthesisIndex = cuePath.rfind(")")
+                cuePath = cuePath.replace(cuePath[cuePathTrackIndex:cuePathParenthesisIndex+1], "")
+                cue = open(cuePath, "a+")
+                cueFiles.append(cuePath)
+                currentGameCuePath = cuePath
             else:
+                if currentGameCue == "":
+                    print("\u001b[1;31mCouldn't find original cue on GitHub.\u001b[0m\n--------")
+                    return None
+                #Use "currentGameCue" instead of fetching a new one to make the operations
+                trackIndex = currentGameCue.find("TRACK " + trackNumber)
+                fileIndex = currentGameCue.rfind("FILE \"", 0, trackIndex)
+                nextFileIndex = currentGameCue.find("FILE \"", trackIndex)
+                trackCueText = currentGameCue[fileIndex:nextFileIndex]
+                trackCueText = replaceCueFileName(trackCueText, fileName)
+                cue = open(currentGameCuePath, "a+")
+            cue.seek(0)
+            if cue.read().find(trackCueText) == -1:
+                cue.write(trackCueText)
+                print("\u001b[1;33mAdded track file to the .cue!\u001b[0m\n--------")
+            else:
+                print("\u001b[1;31mTrack file already present.\u001b[0m")
+            cue.close()                        
+
+        else:
+            #Try to fetch it from two sources, if both fail default to generic
+            cueText = fetchCue(entryName, systemName)
+            if cueText == Exception:
                 if genericCues:
-                    print("\u001b[1;31mCouldn't find database entry, creating generic .cue...\u001b[0m")
+                    print("\u001b[1;31mCouldn't find github entry, creating generic .cue...\u001b[0m")
                     createGenericCue(cuePath, fileName)
                     cueFiles.append(cuePath)
                 else:
-                    print("\u001b[1;31mCouldn't find database entry.\u001b[0m\n--------")
-        else:
-            #Create an entry name based by a fixed offset from the hash
-            print("\u001b[1;33mHash \u001b[1;32m" + fileHash + "\u001b[1;33m matches database!")
-            lineEnd = hashFile.find("\n", foundEntry)
-            entryName = hashFile[int(foundEntry + len(fileHash) + 2):int(lineEnd)]
-            #If the file has multiple tracks, use special conditions
-            if entryName.rfind("Track ") != -1:
-                trackNumber = getTrackNumber(entryName)
-                index = entryName.rfind("(Track ")
-                entryName = entryName.replace(entryName[index-1:], ".cue")
-                try:
-                    cueText = fetchCue(entryName)
-                    trackIndex = cueText.find("TRACK " + trackNumber)
-                    fileIndex = cueText.rfind("FILE \"", 0, trackIndex)
-                    nextFileIndex = cueText.find("FILE \"", trackIndex)
-                    trackCueText = cueText[fileIndex:nextFileIndex]
-                    trackCueText = replaceCueFileName(trackCueText, fileName)
-                    cuePathTrackIndex = cuePath.rfind(" (Track")
-                    cuePathParenthesisIndex = cuePath.rfind(")")
-                    cuePath = cuePath.replace(cuePath[cuePathTrackIndex:cuePathParenthesisIndex+1], "")
-                    cue = open(cuePath, "a+")
-                    if cue.read().find(trackCueText) == -1:
-                        cue.write(trackCueText)
-                        print("\u001b[1;33mAdded track file to the .cue!\u001b[0m\n--------")
-                    else:
-                        print("\u001b[1;33mTrack file already present.\u001b[0m")
-                    cue.close()
-                except Exception:
-                    if genericCues:
-                        print("\u001b[1;31mCouldn't find github entry, creating generic .cue...\u001b[0m")
-                        createGenericCue(cuePath, fileName)
-                        cueFiles.append(cuePath)
-                    else:
-                        print("\u001b[1;31mCouldn't find database entry.\u001b[0m\n--------")
-                    return None
+                    print("\u001b[1;31mCouldn't find original cue on GitHub.\u001b[0m\n--------")
+                return None
 
-
-            else:
-                #Try to fetch it from two sources, if both fail default to generic
-                try:
-                    cueText = fetchCue(entryName)
-                except Exception:
-                    if genericCues:
-                        print("\u001b[1;31mCouldn't find github entry, creating generic .cue...\u001b[0m")
-                        createGenericCue(cuePath, fileName)
-                        cueFiles.append(cuePath)
-                    else:
-                        print("\u001b[1;31mCouldn't find database entry.\u001b[0m\n--------")
-                    return None
-
-                cueText = replaceCueFileName(cueText, fileName)
-                cue = open(cuePath, "w+")
-                cue.write(cueText)
-                cue.close()
-                print("\u001b[36mFetched: \"\u001b[1;33m" + fileName[0:len(fileName) - 4] + ".cue\u001b[36m\"\u001b[0m\n--------")
-                global cueCreatedCounter
-                cueCreatedCounter += 1
+            cueText = replaceCueFileName(cueText, fileName)
+            cue = open(cuePath, "w+")
+            cue.write(cueText)
+            cue.close()
+            print("\u001b[36mFetched: \"\u001b[1;33m" + fileName[0:len(fileName) - 4] + ".cue\u001b[36m\"\u001b[0m\n--------")
+            cueCreatedCounter += 1
     else:
         print("\u001b[1;31mThis file already has a .cue\u001b[0m\n--------")
     cueFiles.append(cuePath)
